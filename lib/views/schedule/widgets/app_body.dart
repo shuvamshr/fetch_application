1bbd45abc23e5c0379f0c45a323228c7d80b7a6e
Shuvam Shrestha <shuvamshrestha408@gmail.com>
2024-08-06 17:13:36 +1000

    add app body widget

---
+++ lib/views/schedule/widgets/app_body.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class FetchAppBody extends StatelessWidget {
+  final List<Widget> children;
+  const FetchAppBody({super.key, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topCenter,
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 500),
+        child: ScrollConfiguration(
+          behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: children,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
