2bdbcc04e2a892fe2b1f4927aa5fbdc9540f4865
Shuvam Shrestha <shuvamshrestha408@gmail.com>
2024-08-06 11:07:46 +1000

    add: colors constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// Primitives
+
+const Color gray_900 = Color(0xFF1B1B1B);
+const Color gray_500 = Color(0xFF79747E);
+const Color gray_300 = Color(0xFFF3F3F3);
+const Color gray_200 = Color(0xFFE8E7EC);
+const Color gray_100 = Color(0xFFF5F5F7);
+
+const Color green_500 = Color(0xFF0A9D62);
+
+const Color black = Color(0xFF000000);
+const Color white = Color(0xFFFFFFFF);
+
+// Tokens
+
+const Color bodyPrimaryBackground = gray_100;
+
+const Color appBarPrimaryBackground = gray_100;
+const Color appBarPrimaryForeground = gray_900;
