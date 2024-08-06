1ddadcbe7e2f744883c400e37478181db305a148
Shuvam Shrestha <shuvamshrestha408@gmail.com>
2024-08-06 11:11:18 +1000

    remove debug banner, add title and add ScheduleView to home

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fetch_application/views/schedule/schedule_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      title: 'Fetch App',
+      debugShowCheckedModeBanner: false,
+      home: ScheduleView(),
     );
   }
 }
