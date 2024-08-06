179ba0e898c8812f0f27fecb6615611c8c14e362
Shuvam Shrestha <shuvamshrestha408@gmail.com>
2024-08-06 17:13:57 +1000

    add tracker card and list widget

---
+++ lib/views/schedule/widgets/tracker_card.dart
@@ -0,0 +1,57 @@
+import 'package:fetch_application/constants/colors.dart';
+import 'package:fetch_application/constants/icons.dart';
+import 'package:fetch_application/constants/typography.dart';
+import 'package:fetch_application/models/tracker_model.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TrackerCard extends StatelessWidget {
+  final Tracker tracker;
+
+  const TrackerCard({super.key, required this.tracker});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: cardPrimaryBackground,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        children: [
+          Stack(
+            alignment: Alignment.bottomRight,
+            children: [
+              CircleAvatar(
+                radius: 52 / 2,
+                backgroundImage:
+                    AssetImage('images/pets/${tracker.pet.image}.png'),
+              ),
+              CircleAvatar(
+                radius: 26 / 2,
+                backgroundImage:
+                    AssetImage('images/category/${tracker.category.image}.png'),
+              ),
+            ],
+          ),
+          const SizedBox(width: 20),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(tracker.category.title, style: cardTitleStyle),
+                Text(tracker.category.description, style: cardSubheadingStyle),
+                const SizedBox(height: 8),
+                Text(DateFormat('h:mma, d MMM yyyy').format(tracker.dateTime),
+                    style: cardFootnoteStyle),
+              ],
+            ),
+          ),
+          const SizedBox(width: 16),
+          rightArrowIcon,
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/views/schedule/widgets/tracker_card_list_view.dart
@@ -0,0 +1,41 @@
+import 'package:fetch_application/constants/typography.dart';
+import 'package:fetch_application/models/tracker_model.dart';
+import 'package:fetch_application/views/schedule/widgets/tracker_card.dart';
+import 'package:flutter/material.dart';
+
+class TrackerCardListView extends StatelessWidget {
+  final String title;
+  final List<Tracker> trackers;
+
+  const TrackerCardListView({
+    super.key,
+    required this.title,
+    required this.trackers,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 8),
+              child: Text(title, style: cardListTitleStyle)),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: ListView.separated(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) =>
+                  TrackerCard(tracker: trackers[index]),
+              separatorBuilder: (context, _) => const SizedBox(height: 12),
+              itemCount: trackers.length,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
