56ed4487738fc0c1ed83ec8d73fbc06a51b25773
Shuvam Shrestha <shuvamshrestha408@gmail.com>
2024-08-06 18:22:37 +1000

    create primary button

---
+++ lib/views/schedule/widgets/primary_button.dart
@@ -0,0 +1,29 @@
+import 'package:fetch_application/constants/colors.dart';
+import 'package:fetch_application/constants/typography.dart';
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String title;
+  final Function() onPressed;
+
+  const PrimaryButton(
+      {super.key, required this.title, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: onPressed,
+        child: Container(
+          height: 52,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: buttonPrimaryBackground,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Text(
+            title,
+            style: buttonTitleStyle,
+          ),
+        ));
+  }
+}
