c3ea7728510b57c0de04bebf341b056ce771009c
Shuvam Shrestha <shuvamshrestha408@gmail.com>
2024-08-06 11:07:39 +1000

    add: icon constants

---
+++ lib/constants/icons.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:fetch_application/constants/colors.dart';
+
+const Icon leftArrowIcon = Icon(
+  Icons.chevron_left,
+  color: gray_900,
+  size: 24,
+);
+
+const Icon rightArrowIcon = Icon(
+  Icons.chevron_right,
+  color: gray_900,
+  size: 24,
+);
+
+const Icon historyIcon = Icon(
+  Icons.history,
+  color: gray_900,
+  size: 24,
+);
+
+const Icon checkmarkIcon = Icon(
+  Icons.check_circle,
+  color: green_500,
+  size: 24,
+);
