7264cacd2b9db51644fb9e7077fed43f42e5a2dd
Shuvam Shrestha <shuvamshrestha408@gmail.com>
2024-08-06 17:13:46 +1000

    add new tracker button

---
+++ lib/views/schedule/widgets/add_tracker_button.dart
@@ -0,0 +1,24 @@
+import 'package:fetch_application/constants/colors.dart';
+import 'package:fetch_application/constants/icons.dart';
+import 'package:flutter/material.dart';
+
+class AddTrackerButton extends StatelessWidget {
+  final Function onPressed;
+  const AddTrackerButton({super.key, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onPressed,
+      child: Container(
+        height: 60,
+        width: 60,
+        decoration: BoxDecoration(
+          color: buttonPrimaryBackground,
+          borderRadius: BorderRadius.circular(999),
+        ),
+        child: addIcon,
+      ),
+    );
+  }
+}
