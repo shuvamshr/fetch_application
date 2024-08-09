d20503b5221e750bd997f216ec32a3093f0df792
Shuvam Shrestha <shuvamshrestha408@gmail.com>
2024-08-09 18:15:22 +1000

    rename to tracker history view

--- lib/views/tracker_view/routes/tracker_history_view.dart
+++ lib/views/tracker_view/routes/tracker_history_view.dart
@@ -2,18 +2,19 @@ import 'package:fetch_application/utils/constants/colors.dart';
 import 'package:fetch_application/utils/constants/icons.dart';
 import 'package:fetch_application/utils/constants/typography.dart';
 
-import 'package:fetch_application/models/tracker_model.dart';
+import 'package:fetch_application/view_models/tracker_view_model.dart';
 import 'package:fetch_application/views/tracker_view/widgets/app_bar.dart';
 import 'package:fetch_application/views/tracker_view/widgets/app_body.dart';
 import 'package:fetch_application/views/tracker_view/widgets/tracker_card_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class ScheduleHistoryView extends StatelessWidget {
-  final List<Tracker> trackers;
-  const ScheduleHistoryView({super.key, required this.trackers});
+class TrackerHistoryView extends StatelessWidget {
+  const TrackerHistoryView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final trackerViewModel = context.watch<TrackerViewModel>();
     return Scaffold(
       backgroundColor: appBodyPrimaryBackground,
       appBar: FetchAppBar(
@@ -28,9 +29,11 @@ class ScheduleHistoryView extends StatelessWidget {
         ],
       ),
       body: FetchAppBody(children: [
-        if (trackers.isNotEmpty)
-          TrackerCardListView(title: "Past For All Pets", trackers: trackers),
-        if (trackers.isEmpty)
+        if (trackerViewModel.trackersByPast.isNotEmpty)
+          TrackerCardListView(
+              title: "Past For All Pets",
+              trackers: trackerViewModel.trackersByPast),
+        if (trackerViewModel.trackersByPast.isEmpty)
           Container(
             alignment: Alignment.center,
             padding: const EdgeInsets.only(top: 128),
